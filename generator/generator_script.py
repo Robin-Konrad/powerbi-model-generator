d337772515611d6450611f482f6c82a07e130b6c
Robin-Konrad <124377116@umail.ucc.ie>
2026-08-28 15:33:15 +0100

    fix bug where wrong function name imported from tmdl_lint.py, and make it exit with code 1 if any problems found

--- generator/generator_script.py
+++ generator/generator_script.py
@@ -3,7 +3,7 @@ import generator.core as core
 from measures_columns import add_fixed_measures_columns
 from globals_table import create_globals_table
 from granularity_parameter import create_date_granularity_parameter
-from tmdl_lint import lint_tables_dir
+from tmdl_lint import lint_run()
 
 def add_date_relationship():
     """
@@ -47,11 +47,12 @@ def main():
     print(f"Script finished running. open the .pbip in Power Bi to see the results")
 
     # run tmdl_lint at the end to quickly find any common errors in the tmdl files
-    problems = lint_tables_dir()
+    problems = lint_run()
     if problems:
         print(f"WARNING: tmdl_lint.py found {len(problems)} issue(s):")
         for p in problems:
             print(f" - {p}")
+        raise SystemExit(1)
 
 
 
